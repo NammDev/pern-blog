ee4b30cc70f3a1303d5c3b0b37e6e547a7046535
Nam <namdeveloper.ca@gmail.com>
2023-02-10 11:38:13 +0700

    Shortcut for import layout & page to routes

---
+++ client/src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as Home } from './Home/Home'
+export { default as Login } from './Login/Login'
+export { default as Register } from './Register/Register'
+export { default as Single } from './Single/Single'
+export { default as Write } from './Write/Write'
--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,11 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom'
-import MainLayout from '~/layouts/MainLayout/MainLayout'
-import FragmentLayout from '~/layouts/FragmentLayout/FragmentLayout'
-import Home from '~/pages/Home/Home'
-import Single from '~/pages/Single/Single'
-import Write from '~/pages/Write/Write'
-import Login from '~/pages/Login/Login'
-import Register from '~/pages/Register/Register'
+import { MainLayout, FragmentLayout } from '~/layouts'
+import { Home, Login, Register, Single, Write } from '~/pages'
 
 const router = createBrowserRouter([
   {
