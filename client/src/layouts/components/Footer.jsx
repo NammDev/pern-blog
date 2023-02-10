408952d8e6a07a22e8186763594753e4926a4739
Nam <namdeveloper.ca@gmail.com>
2023-02-10 15:15:04 +0700

    Style Footer

--- client/src/layouts/components/Footer.jsx
+++ client/src/layouts/components/Footer.jsx
@@ -1,5 +1,19 @@
-function Footer() {
-  return <div>Footer</div>
+import React from 'react'
+import Logo from '~/assets/img/logo.png'
+import classNames from 'classnames/bind'
+import styles from './Footer.module.scss'
+
+const cx = classNames.bind(styles)
+
+const Footer = () => {
+  return (
+    <footer className={cx('footer')}>
+      <img src={Logo} alt='' />
+      <span>
+        Made with ♥️ and <b>React.js</b>.
+      </span>
+    </footer>
+  )
 }
 
 export default Footer
