aba93ef71faec431571eed20ddec08aa8fa17d6e
Nam <namdeveloper.ca@gmail.com>
2023-02-10 15:25:39 +0700

    Style Home Page

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,63 @@
 import classNames from 'classnames/bind'
 import styles from './Home.module.scss'
+import { Link } from 'react-router-dom'
+import Button from '~/components/Button/Button'
 
 const cx = classNames.bind(styles)
 
 function Home() {
-  return <div className={cx('home')}>Hello</div>
+  const posts = [
+    {
+      id: 1,
+      title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+      desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
+      img: 'https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    },
+    {
+      id: 2,
+      title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+      desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
+      img: 'https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    },
+    {
+      id: 3,
+      title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+      desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
+      img: 'https://images.pexels.com/photos/4230630/pexels-photo-4230630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    },
+    {
+      id: 4,
+      title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+      desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
+      img: 'https://images.pexels.com/photos/6157049/pexels-photo-6157049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    },
+  ]
+
+  const getText = (html) => {
+    const doc = new DOMParser().parseFromString(html, 'text/html')
+    return doc.body.textContent
+  }
+
+  return (
+    <div className={cx('home')}>
+      <div className={cx('posts')}>
+        {posts.map((post) => (
+          <div className={cx('post')} key={post.id}>
+            <div className={cx('img')}>
+              <img src={post.img} alt='' />
+            </div>
+            <div className={cx('content')}>
+              <Link to={`/post/${post.id}`}>
+                <h1>{post.title}</h1>
+              </Link>
+              <p>{getText(post.desc)}</p>
+              <Button secondary text='Read More' />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Home
