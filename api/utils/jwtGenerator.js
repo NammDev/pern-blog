e5bf349588dc8b2e3d9620b30ac6cfdfdbe76d05
Nam <namdeveloper.ca@gmail.com>
2023-02-11 23:02:12 +0700

    Create sample jwt genetator

---
+++ api/utils/jwtGenerator.js
@@ -0,0 +1,14 @@
+import jwt from 'jsonwebtoken'
+require('dotenv').config()
+
+function jwtGenerator(id, phone) {
+  const payload = {
+    user: {
+      id,
+      phone,
+    },
+  }
+  return jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: '1d' })
+}
+
+module.exports = jwtGenerator
