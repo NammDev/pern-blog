f60ae35777b9792f08b64f10c9cad6d8a3853d8d
Nam <namdeveloper.ca@gmail.com>
2023-02-11 20:26:01 +0700

    init express & create config

--- api/app.js
+++ api/app.js
@@ -1,1 +1,17 @@
-console.log('s')
+// Setup
+const express = require('express')
+const app = express()
+const config = require('./config')[process.env.NODE_ENV || 'development']
+const log = config.log()
+
+// Connect DB
+
+// Middleware
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// Listen
+port = process.env.PORT || 8800
+app.listen(port, () => {
+  log.info(`Api app listening on port ${port}`)
+})
---
+++ api/config/index.js
@@ -0,0 +1,39 @@
+// Load package.json
+const pjs = require('../package.json')
+const bunyan = require('bunyan')
+
+// Get some meta info from the package.json
+const { name, version } = pjs
+
+const getLogger = (serviceName) => bunyan.createLogger({ name: `${serviceName}` })
+
+module.exports = {
+  development: {
+    name,
+    version,
+    serviceTimeout: 30,
+    sql: {
+      options: {
+        host: 'localhost',
+        port: 5432,
+        database: 'sequelize_course',
+        dialect: 'postgres',
+        username: 'postgres',
+        password: 'namkhanh',
+      },
+    },
+    log: () => getLogger(name),
+  },
+  production: {
+    name,
+    version,
+    serviceTimeout: 30,
+    log: () => getLogger(name),
+  },
+  test: {
+    name,
+    version,
+    serviceTimeout: 30,
+    log: () => getLogger(name),
+  },
+}
