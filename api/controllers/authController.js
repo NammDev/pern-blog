71ac13136ade4fae044976a24007b62aacfc9fcf
Nam <namdeveloper.ca@gmail.com>
2023-02-13 11:20:07 +0700

    build api register

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,32 +1,28 @@
 import bcrypt from 'bcryptjs'
-import { getAllUsers, getUserByEmail } from '../services/authService.js'
+import { getUserByEmail, getUserByEmailName, createNewUser } from '../services/authService.js'
 import { jwtGeneratorAccess, jwtGeneratorRefresh } from '../utils/jwtGenerator.js'
 
 export const register = async (req, res) => {
   try {
-    const rows = await getAllUsers()
-    res.json(rows)
-    // // 1. Destructure the req.body (name, email, password)
-    // const { name, email, password } = req.body
-    // // 2. Check if user exist (if true throw error)
-    // const user = await pool.query('SELECT * FROM users WHERE user_email = $1', [email])
-    // if (user.rows.length !== 0) {
-    //   return res.status(401).json('User already exist')
-    // }
-    // // 3. Bcrypt the user password
-    // const salt = await bcrypt.genSalt(10)
-    // const hash = await bcrypt.hash(password, salt)
-    // // 4. Insert the new user in side our database
-    // const newUser = await pool.query(
-    //   'INSERT INTO users (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *',
-    //   [name, email, hash]
-    // )
-    // // 5. Generating the jwt token
-    // const token = jwtGenerator(newUser.rows[0].user_id)
-    // // 6. Send respond
-    // res.json({ accessToken: token })
+    // 1. Destructure the req.body (name, email, password)
+    const { username, email, password } = req.body
+
+    // 2. Check if user exist (if false throw error)
+    const user = await getUserByEmailName(email, username)
+    if (user.length) return res.status(404).json('Email or Username is already exist!')
+
+    // 3. Bcrypt the user password
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+
+    // 4. Insert the new user in side our database
+    await createNewUser(username, email, hash)
+
+    // 5. Send respond
+    return res.status(200).json('User has been created.')
   } catch (error) {
     console.log(error)
+    res.status(500).json(error)
   }
 }
 
@@ -43,15 +39,15 @@ export const login = async (req, res) => {
     const validPassword = await bcrypt.compare(password, user[0].password)
     if (!validPassword) return res.status(401).json('Password is incorrect')
 
-    // // 4. Give them jwt token
+    // 4. Give them jwt token
     const accessToken = jwtGeneratorAccess(user[0].id)
     const refreshToken = jwtGeneratorRefresh(user[0].id)
 
-    // // 5. Send respond
+    // 5. Send respond
     res.json({ accessToken, refreshToken, ...user[0] })
   } catch (error) {
     console.log(error)
-    res.status(500).send(error)
+    res.status(500).json(error)
   }
 }
 
--- api/services/authService.js
+++ api/services/authService.js
@@ -10,6 +10,31 @@ export const getUserByEmail = (email) =>
     })
   })
 
+export const getUserByEmailName = (email, username) =>
+  new Promise((resolve, reject) => {
+    const q = 'SELECT * FROM user WHERE email = ? OR username = ?'
+    db.query(q, [email, username], (err, rows) => {
+      if (err) {
+        reject(err)
+      } else resolve(rows)
+    })
+  })
+
+export const createNewUser = (username, email, hash) =>
+  new Promise((resolve, reject) => {
+    const q = `INSERT INTO user SET ?`
+    const data = {
+      username,
+      email,
+      password: hash,
+    }
+    db.query(q, data, (err, rows) => {
+      if (err) {
+        reject(err)
+      } else resolve(rows)
+    })
+  })
+
 export const getAllUsers = () =>
   new Promise((resolve, reject) => {
     const q = 'SELECT * FROM user'
