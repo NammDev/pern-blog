f7cbf8901dbb784a64123c798a2d29a1ad4faf1e
Nam <namdeveloper.ca@gmail.com>
2023-02-14 15:32:16 +0700

    single page with fetch post by id & delete post

--- client/src/pages/Single/Single.jsx
+++ client/src/pages/Single/Single.jsx
@@ -1,37 +1,65 @@
+import { useState, useEffect, useContext } from 'react'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
+import DOMPurify from 'dompurify'
 import classNames from 'classnames/bind'
 import styles from './Single.module.scss'
-import { Link } from 'react-router-dom'
 import Menu from './Menu'
-import DOMPurify from 'dompurify'
+import { deletePost, getPost } from '~/services/post'
+import { AuthContext } from '~/context/authContext'
+import moment from 'moment'
+import Edit from '~/assets/img/edit.png'
+import Delete from '~/assets/img/delete.png'
 const cx = classNames.bind(styles)
 
 function Single() {
-  const post = {
-    id: 1,
-    username: 'John Doe',
-    title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
-    desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis! Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis! Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis! Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis! Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis! Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
-    img: 'https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    userImg:
-      'https://images.pexels.com/photos/4230630/pexels-photo-4230630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+  const [post, setPost] = useState({})
+  const location = useLocation()
+  const navigate = useNavigate()
+
+  const postId = location.pathname.split('/')[2]
+
+  const { currentUser } = useContext(AuthContext)
+
+  const handleDelete = async () => {
+    try {
+      await deletePost(postId)
+      navigate('/')
+    } catch (err) {
+      console.log(err)
+    }
   }
+
+  const fetchData = async () => {
+    try {
+      const data = await getPost(postId)
+      setPost(data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [postId])
 
   return (
     <div className={cx('single')}>
       <div className={cx('content')}>
         <img src={post.img} alt='' />
         <div className={cx('user')}>
-          <img src={post.userImg} alt='' />
+          {post.userImg && <img src={post.userImg} alt='' />}
           <div className='info'>
             <span>{post.username}</span>
-            <p>Posted 2 days ago</p>
+            <p>Posted {moment(post.date).fromNow()}</p>
           </div>
-          <div className={cx('edit')}>
-            <Link to={`/write?edit=2`}>
-              <img alt='' />
-            </Link>
-            <img alt='' />
-          </div>
+          {currentUser.username === post.username && (
+            <div className={cx('edit')}>
+              <Link to={`/write?edit=2`} state={post}>
+                <img src={Edit} alt='' />
+              </Link>
+              <img onClick={handleDelete} src={Delete} alt='' />
+            </div>
+          )}
         </div>
         <h1>{post.title}</h1>
         <p
--- client/src/services/post.js
+++ client/src/services/post.js
@@ -4,3 +4,13 @@ export const getPosts = async (cat) => {
   const res = await axios.get(`/api/posts${cat}`)
   return res
 }
+
+export const getPost = async (id) => {
+  const res = await axios.get(`/api/posts/${id}`)
+  return res
+}
+
+export const deletePost = async (id) => {
+  const res = await axios.delete(`/api/posts/${id}`)
+  return res
+}
